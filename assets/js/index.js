2d93f5b095402efd22381b7835658d9a4e9e13a0
ida <the.secret.lyrics.20316143@gmail.com>
2025-10-16 18:53:57 +0900

    VueからReactに換装。画像追加

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,76 +1,86 @@
-const items = [
-  {'Id':'', 'Galar':'', 'Alt':'', 'Estimation':'', 'Japanese':'', 'Image':''},
-];
+const { useMemo, useState } = React;
 
-new Vue({
-  el: '#app',
-  data: {
-    items: items,
-    sort: {
-      key: '', // ソートキー
-      isAsc: false // 昇順ならtrue,降順ならfalse
-    },
-    // types: types,       // チェックボックスの値
-    // selectTypes: types,  // 選択されたチェックボックスの値
-    searchKalos: '',   // 入力された文字列を格納
-  },
-  computed: {
-    eventedAction: function() {
-      let list = this.items.slice();
+function App() {
+  const [items, setItems] = useState(() => (window.KalosItems || []).slice());
+  const [sort, setSort] = useState({ key: '', isAsc: false });
+  const [searchKalos, setSearchKalos] = useState('');
 
-      // ソート実施
-      if(this.sort.key) {
-        list.sort((a, b) => {
-          a = a[this.sort.key];
-          b = b[this.sort.key];
-          return (a === b ? 0 : a > b ? 1 : -1) * (this.sort.isAsc ? 1 : -1);
-        });
-      }
-      // Nameで検索実施
-      if (this.searchKalos) {
-        list = list.filter(element => {
-          return Object.keys(element).some(key => {
-            if(key === 'Kalos') {
-              return element[key].indexOf(this.searchKalos) > -1;
-            }
-          });
-        });
-      }
-
-      return list;
-    }   
-  },
-  methods: {
-    // sort用キーをセットし、昇順・降順を入れ替える
-    sortBy: function(key) {
-      this.sort.isAsc = this.sort.key === key ? !this.sort.isAsc : false;
-      this.sort.key = key;
-    },
-    sortedClass: function(key) {
-      return this.sort.key === key ? `sorted ${this.sort.isAsc ? 'asc' : 'desc' }` : '';
-    },
-    // 全ての処理をクリアする
-    resetting: function() {
-      this.sort.key = '';
-      this.sort.isAsc = false;
-      // this.selectTypes = types;
-      this.searchKalos = '';
-      this.items = items;
+  const list = useMemo(() => {
+    let next = items.slice();
+    if (sort.key) {
+      next.sort((a, b) => {
+        const av = a[sort.key];
+        const bv = b[sort.key];
+        return (av === bv ? 0 : av > bv ? 1 : -1) * (sort.isAsc ? 1 : -1);
+      });
     }
-  }
-});
-! function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    p = /^http:/.test(d.location) ? 'http' : 'https';
-  if (!d.getElementById(id)) {
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p + '://platform.twitter.com/widgets.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }
-}(document, 'script', 'twitter-wjs');
+    if (searchKalos) {
+      next = next.filter((element) => String(element.Kalos || '').indexOf(searchKalos) > -1);
+    }
+    return next;
+  }, [items, sort, searchKalos]);
 
-$('p.galar').css({'white-space':'pre-line'});
-$('p.hisui').css({'white-space':'pre-line'});
-$('p.kalos').css({'white-space':'pre-line'});
-$('p.text').css({'white-space':'pre-line'});
+  const sortBy = (key) => {
+    setSort((prev) => ({ key, isAsc: prev.key === key ? !prev.isAsc : false }));
+  };
+
+  const sortedClass = (key) => (sort.key === key ? `sorted ${sort.isAsc ? 'asc' : 'desc'}` : '');
+
+  const resetting = () => {
+    setSort({ key: '', isAsc: false });
+    setSearchKalos('');
+    setItems((window.KalosItems || []).slice());
+  };
+
+  React.useEffect(() => {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    })(document, 'script', 'twitter-wjs');
+  }, []);
+
+  return (
+    React.createElement('div', null,
+      React.createElement('table', null,
+        React.createElement('thead', null,
+          React.createElement('tr', null,
+            React.createElement('th', { onClick: () => sortBy('Image'), className: sortedClass('Image') }, 'SS'),
+            React.createElement('th', { onClick: () => sortBy('Kalos'), className: sortedClass('Kalos') }, 'カロス文字'),
+            React.createElement('th', { onClick: () => sortBy('Estimation'), className: sortedClass('Estimation') }, '推定される文字'),
+            React.createElement('th', { onClick: () => sortBy('Japanese'), className: sortedClass('Japanese') }, '日本語')
+          )
+        ),
+        React.createElement('tbody', null,
+          list.map((row) => (
+            React.createElement('tr', { key: row.Id },
+              React.createElement('td', null,
+                React.createElement('a', { 'data-fancybox': 'gallery', href: row.Image, 'data-caption': row.Alt },
+                  React.createElement('img', { src: row.Image, alt: row.Alt })
+                )
+              ),
+              React.createElement('td', null,
+                React.createElement('p', { className: 'galar' }, row.Galar),
+                React.createElement('p', { className: 'hisui' }, row.Hisui),
+                React.createElement('p', { className: 'kalos' }, row.Kalos),
+                React.createElement('p', { className: 'text' }, row.Alt),
+                React.createElement('p', { className: 'text' }, row.Text)
+              ),
+              React.createElement('td', null),
+              React.createElement('td', null)
+            )
+          ))
+        )
+      )
+    )
+  );
+}
+
+const ReactDOMClient = window.ReactDOMClient || window.ReactDOM;
+ReactDOMClient.createRoot(document.getElementById('app')).render(React.createElement(App));
+
---
+++ assets/js/items.js
@@ -0,0 +1,9 @@
+const items = [
+  { 'Id': '1', 'Kalos': 'A', 'Alt': '', 'Estimation': '', 'Japanese': 'ミアレシティ', 'Image': './assets/images/story_01.webp' },
+  { 'Id': '2', 'Kalos': 'B', 'Alt': '', 'Estimation': '', 'Japanese': '', 'Image': './assets/images/story_02.webp' },
+  { 'Id': '3', 'Kalos': 'C', 'Alt': '', 'Estimation': '', 'Japanese': '', 'Image': './assets/images/story_03.webp' },
+];
+
+window.KalosItems = items;
+
+
